353d5c53804bfc5e95dcb69aa5056375c89f28bc
Cami Diez <diezcami@gmail.com>
2015-09-05 19:58:58 +0800

    init

---
+++ GameModel.swift
@@ -0,0 +1,13 @@
+//
+//  GameModel.swift
+//  Match
+//
+//  Created by Cami on 9/5/15.
+//  Copyright (c) 2015 Camille Diez. All rights reserved.
+//
+
+import UIKit
+
+class GameModel: NSObject {
+   
+}
---
+++ Match/Card.swift
@@ -0,0 +1,21 @@
+//
+//  Card.swift
+//  Match
+//
+//  Created by Cami on 9/5/15.
+//  Copyright (c) 2015 Camille Diez. All rights reserved.
+//
+
+import UIKit
+
+class Card: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
