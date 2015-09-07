393d9a7409f5322faff17d845eaf5919c7796b31
Cami Diez <diezcami@gmail.com>
2015-09-08 01:26:36 +0800

    Added readability tweaks

--- Match/ViewController.swift
+++ Match/ViewController.swift
@@ -72,8 +72,6 @@ class ViewController: UIViewController {
                 
                 // Add constraint
                 self.contentView.addConstraint(leftMarginConstraint)
-                
-                
             }
             
             // Set the vertical position
@@ -117,16 +115,20 @@ class ViewController: UIViewController {
                 self.flipDownAllCards()
                 // Flip card up
                 cardThatWasTapped.flipUp()
+                // Add first card
                 self.revealedCard = cardThatWasTapped
-                
             } else { // Second Card
                 // Flip card up
                 cardThatWasTapped.flipUp()
+                
+                // Check for matching card values
                 if (revealedCard?.cardValue == cardThatWasTapped.cardValue) { // Cards are the same
                     // Hide both cards
                     self.revealedCard?.hideCard()
                     cardThatWasTapped.hideCard()
                 } // else do nothing
+                
+                // Set revealed card back to null
                 self.revealedCard = nil
             }
         } // If it's flipped, do nothing
